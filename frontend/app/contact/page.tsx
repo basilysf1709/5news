5408f6a023d8acfd0fac0ad4299773df0dc07af3
basilysf1709 <basilyusuf1709@gmail.com>
2024-01-23 11:01:51 -0500

    upgrade

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,11 +1,11 @@
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 
-export default function Home() {
+export default function Contact() {
   return (
     <>
       <Navbar />
-      <form action="#" className="mt-10 mb-40 mr-40 ml-40">
+      <form action="#" className="mt-10 mb-10 mr-10 ml-10 md:mt-10 md:mb-40 md:mr-40 md:ml-40">
         <div className="mb-10">
           <label
             htmlFor="email"
@@ -52,7 +52,7 @@ export default function Home() {
         </div>
         <button
           type="submit"
-          className="text-white bg-dark-custom-blue hover:bg-blue-500 w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none block"
+          className="bg-primary hover:bg-secondary w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none block"
         >
           Send message
         </button>
